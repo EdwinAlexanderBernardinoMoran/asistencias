9ce84ed9bc63065d19c6ff0e28b4169d1274c439
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-09-16 20:23:22 -0600

    Creando Menu de navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    SharedModule,
     NgbModule
   ],
   providers: [],
