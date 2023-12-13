85e1a7789e8fb671c8c898fdea8d3a6130f90da2
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-12-12 21:19:46 -0600

    Mostrando datos del estudiante desde la API de asistencias

--- src/app/student/pages/home-student-page/home-student-page.component.ts
+++ src/app/student/pages/home-student-page/home-student-page.component.ts
@@ -4,25 +4,6 @@ import { StudentService } from '../../services/student.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface Person {
-  id: number;
-  name: string;
-  lastname: string;
-  nie: number;
-  especialidad: string;
-}
-
-const ELEMENT_DATA: Person[] = [
-  {id: 1, name: 'Edwin Alexander', lastname: 'Bernardino Moran', nie: 1.0079, especialidad: 'Desarrollo De Software'},
-  {id: 2, name: 'Benjamin Ernesto', lastname: 'Moran Mate', nie: 4.0026, especialidad: 'Ingenierio en Sistemas Informaticos'},
-  {id: 3, name: 'Karla Ibeth', lastname: 'Cruz Moran', nie: 6.941, especialidad: 'Administrativo Contable'},
-  {id: 4, name: 'Maria Magdalena', lastname: 'Moran Mate', nie: 9.0122, especialidad: 'Atencion Primaria En Salud'},
-  {id: 5, name: 'Jonathan Alexander', lastname: 'Bernardino Moran', nie: 10.811, especialidad: 'Desarrollo De Software'},
-  {id: 5, name: 'Alfredo Adonay', lastname: 'Casio Moran', nie: 10.811, especialidad: 'Desarrollo De Software'},
-  {id: 5, name: 'Moises Adonay', lastname: 'Bonilla Alvarenga', nie: 10.811, especialidad: 'Desarrollo De Software'},
-  {id: 5, name: 'Abdiaz De Jesus', lastname: 'Martinez Hernandez', nie: 10.811, especialidad: 'Desarrollo De Software'}
-];
-
 @Component({
   selector: 'student-home-student-page',
   templateUrl: './home-student-page.component.html',
@@ -32,25 +13,25 @@ export class HomeStudentPageComponent implements OnInit, AfterViewInit {
 
   public title:string = "Listado De Estudiantes";
 
-  public displayedColumns: string[] = ['id', 'name', 'lastname', 'nie', 'especialidad', 'acciones'];
-  public dataSource: MatTableDataSource<Person>;
+  public displayedColumns: string[] = ['id', 'names', 'lastnames', 'nie', 'incomeSpecialty', 'acciones'];
+  public student: MatTableDataSource<Student>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  public student: Student[] = [];
+  // public student: Student[] = [];
   public links: Links | null = null;
 
   constructor(private homeStudent: StudentService){
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA)
+    this.student = new MatTableDataSource()
   }
   ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
+    this.student.paginator = this.paginator;
   }
 
   ngOnInit(): void {
     this.homeStudent.getStudent().subscribe(
       (response: SearchStudent) => {
-        this.student = response.data;
+        this.student.data = response.data
         this.links = response.links;
       }
     )
