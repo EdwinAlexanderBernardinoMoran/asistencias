7591ff9ebbcd56c4c88e1259aa6110f9dcf8b0ad
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-02-02 20:22:40 -0600

    FormReactive de estudiantes, agregando cada campo del formulario dentro del componente (create)

--- src/app/student/pages/create-student-page/create-student-page.component.ts
+++ src/app/student/pages/create-student-page/create-student-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 export interface Hamlet{
   id: number,
@@ -22,7 +22,89 @@ export class CreateStudentPageComponent {
 
   hide = true;
 
-  disableSelect = new FormControl(false);
-
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  public studentForm = new FormGroup({
+    names: new FormControl(''),
+    lastnames: new FormControl(''),
+    dateBirth: new FormControl(''),
+    nationality_id: new FormControl(''),
+    departmentBirth_id: new FormControl(''),
+    municipalityBirth_id: new FormControl(''),
+    yearStudy: new FormControl(''),
+    nie: new FormControl(''),
+    departureNumber: new FormControl(''),
+    departureFolio: new FormControl(''),
+    departureBook: new FormControl(''),
+    anotherIdentificationDocument: new FormControl(''),
+    salvadoreno_por: new FormControl(''),
+    incomeSpecialty_id: new FormControl(''),
+    parvularianStudy: new FormControl(''),
+    repeatSection: new FormControl(''),
+    school_center_id: new FormControl(''),
+    previousYear: new FormControl(''),
+    bloodType: new FormControl(''),
+    sexo: new FormControl(''),
+    stateFamiliar: new FormControl(''),
+    disability: new FormControl(''),
+    email: new FormControl(''),
+    telephone: new FormControl(''),
+    zone_id: new FormControl(''),
+    departmentResidence_id: new FormControl(''),
+    municipalityResidence_id: new FormControl(''),
+    cantonResidence_id: new FormControl(''),
+    hamletResidence_id: new FormControl(''),
+    streetType: new FormControl(''),
+    fullAddress: new FormControl(''),
+    middleTransport: new FormControl(''),
+    distanceFromHomeSchool: new FormControl(''),
+    riskFactor: new FormControl(''),
+    numberMembersFamily: new FormControl(''),
+    integrated: new FormControl(''),
+    dependsEconomically: new FormControl(''),
+    hasChildren: new FormControl(''),
+    numberChildren: new FormControl(''),
+    work: new FormControl(''),
+    coexistenceWith: new FormControl(''),
+    mothersName: new FormControl(''),
+    occupationMother: new FormControl(''),
+    workplaceOfTheMother: new FormControl(''),
+    mothersPhone: new FormControl(''),
+    fathersName: new FormControl(''),
+    occupationFather: new FormControl(''),
+    workplaceOfTheFather: new FormControl(''),
+    fathersPhone: new FormControl(''),
+    ResponsibleNames: new FormControl(''),
+    ResponsibleLastNames: new FormControl(''),
+    DuiResponsible: new FormControl(''),
+    familyStateResponsible: new FormControl(''),
+    emailResponsible: new FormControl(''),
+    phoneResponsible: new FormControl(''),
+    zoneReponsible_id: new FormControl(''),
+    departmentReponsible_id: new FormControl(''),
+    municipalityReponsible_id: new FormControl(''),
+    cantonReponsible_id: new FormControl(''),
+    hamletReponsible_id: new FormControl(''),
+    streetTypeReponsible: new FormControl(''),
+    fullAddressResponsible: new FormControl(''),
+    professionIfficeResponsible: new FormControl(''),
+    responsibleSchool: new FormControl(''),
+    riskFactorResponsable: new FormControl(''),
+    dateReviewForm: new FormControl(''),
+    birthCertificate: new FormControl(''),
+    certificationNotes: new FormControl(''),
+    certificate: new FormControl(''),
+    photos: new FormControl(''),
+    RecordNotes: new FormControl(''),
+    residentCard: new FormControl(''),
+    teacher_id: new FormControl(''),
+  });
+
+  onSubmit():void {
+    console.log({
+      formIsValid: this.studentForm.valid,
+      value: this.studentForm.value
+    });
+
+  }
 }
